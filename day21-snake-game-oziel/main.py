275b1300f8e4c23dc8dda188529958a7f17f7693
ozysouza <ozysouzaa@gmail.com>
2024-05-13 23:53:24 -0400

    Add files for snake game in python

---
+++ day21-snake-game-oziel/main.py
@@ -0,0 +1,47 @@
+from turtle import Screen
+from modules.snake import Snake
+from modules.food import Food
+from modules.scoreboard import Scoreboard
+import time
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game!")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.move_up, "Up")
+screen.onkey(snake.move_down, "Down")
+screen.onkey(snake.move_left, "Left")
+screen.onkey(snake.move_right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+    # Detect collision with wall
+    if snake.collision_on_x() or snake.collision_on_y():
+        game_is_on = False
+        score.game_over("Game is Over! You hit the wall!")
+
+    # Detect collision with body
+    if snake.collision_on_body():
+        game_is_on = False
+        score.game_over("Game is Over! You hit the tail!")
+
+screen.exitonclick()
+
---
+++ day21-snake-game-oziel/modules/snake.py
@@ -0,0 +1,64 @@
+from turtle import Turtle, Screen
+
+START_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.snakes = []
+        self.create_snake()
+        self.head = self.snakes[0]
+
+    def create_snake(self):
+        for position in START_POSITIONS:
+            self.add_snake(position)
+
+    def add_snake(self, position):
+        new_snake = Turtle("square")
+        new_snake.color("white")
+        new_snake.penup()
+        new_snake.goto(position)
+        self.snakes.append(new_snake)
+
+    def extend(self):
+        self.add_snake(self.snakes[-1].position())
+
+    def move(self):
+        for s_index in range(len(self.snakes) - 1, 0, -1):
+            new_x = self.snakes[s_index - 1].xcor()
+            new_y = self.snakes[s_index - 1].ycor()
+            self.snakes[s_index].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def collision_on_x(self):
+        return self.head.xcor() > 290 or self.head.xcor() < -290
+
+    def collision_on_y(self):
+        return self.head.ycor() > 290 or self.head.ycor() < -290
+
+    def collision_on_body(self):
+        for snake in self.snakes[1:]:
+            if self.head.distance(snake) < 10:
+                return True
+
+    def move_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def move_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def move_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def move_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
