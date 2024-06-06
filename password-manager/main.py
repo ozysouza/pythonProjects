88a3516f3c05045912f8b1c2f70adde5d0b90c28
ozysouza <ozysouzaa@gmail.com>
2024-06-05 22:17:31 -0400

    Add small project about password-manager

---
+++ password-manager/main.py
@@ -0,0 +1,73 @@
+from tkinter import *
+from tkinter import messagebox
+import secrets
+import pyperclip
+
+window = Tk()
+window.title("Password Manager")
+window.config(pady=40, padx=40, bg="white")
+
+
+# ------------------------ Generate Password -------------------------
+def random_passwd():
+    passwd_len = 8
+    secret_passwd = secrets.token_urlsafe(passwd_len)
+    if len(passwd_entry.get()) > 0:
+        passwd_entry.delete(0, END)
+
+    passwd_entry.insert(END, string=f"{secret_passwd}")
+    pyperclip.copy(secret_passwd)
+
+
+# ------------------------ Save Password -------------------------
+def save():
+    if len(website_entry.get()) == 0 or len(passwd_entry.get()) == 0:
+        messagebox.showinfo(title="Opps!", message="Please don't leave empty fields!")
+    else:
+        save_data = messagebox.askokcancel(title=website_entry.get(), message=f"There are the details entered: "
+                                                                              f"\nEmail: {username_entry.get()}"
+                                                                              f"\nPassword: {passwd_entry.get()}")
+
+        if save_data:
+            with open("data.txt", "a") as file:
+                file.write(f"{website_entry.get()} | {username_entry.get()} | {passwd_entry.get()} \n")
+
+                website_entry.delete(0, END)
+                website_entry.focus()
+                passwd_entry.delete(0, END)
+
+
+# Set Up Screen
+
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:", bg="white")
+website_label.grid(row=1, column=0)
+
+website_entry = Entry(width=40)
+website_entry.grid(row=1, column=1, columnspan=2, padx=2, pady=2)
+website_entry.focus()
+
+username_label = Label(text="Email/Username:", bg="white")
+username_label.grid(row=2, column=0)
+
+username_entry = Entry(width=40)
+username_entry.grid(row=2, column=1, columnspan=2, padx=2, pady=2)
+username_entry.insert(0, "test@gmail.com")
+
+passwd_label = Label(text="Password:", bg="white")
+passwd_label.grid(row=3, column=0)
+
+passwd_entry = Entry(width=23)
+passwd_entry.grid(row=3, column=1, padx=2, pady=2)
+
+generate_passwd_btn = Button(text="Generate Password", width=13, bg="white", command=random_passwd)
+generate_passwd_btn.grid(row=3, column=2, padx=2, pady=2)
+
+add_btn = Button(text="Add", width=38, bg="white", command=save)
+add_btn.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
