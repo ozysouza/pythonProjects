fd329844d25b06723154a98371632d3577bac2c4
ozysouza <ozysouzaa@gmail.com>
2024-06-20 23:03:31 -0400

    Modify quiz project

--- quiz-project/main.py
+++ quiz-project/main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 for question in question_data:
@@ -10,8 +11,4 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-
-while quiz.still_has_questions():
-    quiz.next_question()
-
-quiz.final_score()
+quiz_ui = QuizInterface(quiz)
